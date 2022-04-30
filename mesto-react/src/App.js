ecfa1cd3c41671e18b28337f7e135be7acf6ffd2
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-04-30 14:54:24 +0500

    добавить стили в проект

--- mesto-react/src/App.js
+++ mesto-react/src/App.js
@@ -1,70 +1,70 @@
 import React from 'react';
-import './App.css';
+import './index.css'
 
 function App() {
   return (
     <div className="page">
-      <header class="header">
+      <header className="header">
         <img
-          class="header__logo"
+          className="header__logo"
           alt="лого"
           src="<%=require('./images/header__logo.svg')%>"
         />
       </header>
 
-      <main class="content">
-        <section class="profile">
-          <div class="profile__account">
-            <div class="profile__avatar-container">
+      <main className="content">
+        <section className="profile">
+          <div className="profile__account">
+            <div className="profile__avatar-container">
             <img
-              class="profile__avatar"
+              className="profile__avatar"
               alt="аватар"
               src="<%=require('./images/profile__photo.jpg')%>"
             />
             <button type="button" class="profile__avatar-edit-btn"></button>
           </div>
-            <div class="profile__info">
-              <div class="profile__name-box">
-                <h1 class="profile__name">Жак-Ив Кусто</h1>
+            <div className="profile__info">
+              <div className="profile__name-box">
+                <h1 className="profile__name">Жак-Ив Кусто</h1>
                 <button
                   aria-label="изменить профиль"
-                  class="profile__edit-button"
+                  className="profile__edit-button"
                   type="button"
                 ></button>
               </div>
-              <p class="profile__description">Исследователь океана</p>
+              <p className="profile__description">Исследователь океана</p>
             </div>
           </div>
           <button
             aria-label="добавить фото"
-            class="profile__add-button"
+            className="profile__add-button"
             type="button"
           ></button>
         </section>
-        <section class="elements"></section>
+        <section className="elements"></section>
       </main>
 
-      <footer class="footer">
-        <p class="footer__copyright">&copy; 2022 Mesto Russia</p>
+      <footer className="footer">
+        <p className="footer__copyright">&copy; 2022 Mesto Russia</p>
       </footer>
 
-      <div class="popup popup_type_edit-profile">
-        <div class="popup__container popup__container_type_edit-profile">
+      <div className="popup popup_type_edit-profile">
+        <div className="popup__container popup__container_type_edit-profile">
           <button
-            class="popup__close-button popup__close-button_type_edit-profile"
+            className="popup__close-button popup__close-button_type_edit-profile"
             type="button"
           ></button>
-          <h3 class="popup__heading">Редактировать профиль</h3>
+          <h3 className="popup__heading">Редактировать профиль</h3>
           <form
-            class="popup__form popup__form_type_edit-profile"
+            className="popup__form popup__form_type_edit-profile"
             name="profile-info"
             novalidate
           >
-            <div class="popup__input-container">
+            <div className="popup__input-container">
               <input
                 name="name"
                 id="name-input"
-                class="popup__input popup__input_type_name"
+                className="popup__input popup__input_type_name"
                 type="text"
                 placeholder="Имя"
                 minlength="2"
@@ -73,14 +73,14 @@ function App() {
               />
               <span
                 id="name-input-error"
-                class="popup__input-error popup__input-error_visible"
+                className="popup__input-error popup__input-error_visible"
               ></span>
             </div>
-            <div class="popup__input-container">
+            <div className="popup__input-container">
               <input
                 name="job"
                 id="job-input"
-                class="popup__input popup__input_type_description"
+                className="popup__input popup__input_type_description"
                 type="text"
                 placeholder="Деятельность"
                 minlength="2"
@@ -89,31 +89,31 @@ function App() {
               />
               <span
                 id="job-input-error"
-                class="popup__input-error popup__input-error_visible"
+                className="popup__input-error popup__input-error_visible"
               ></span>
             </div>
-            <button class="popup__save-button" type="submit">Сохранить</button>
+            <button className="popup__save-button" type="submit">Сохранить</button>
           </form>
         </div>
       </div>
 
-      <div class="popup popup_type_add-card">
-        <div class="popup__container popup__container_type_add-card">
+      <div className="popup popup_type_add-card">
+        <div className="popup__container popup__container_type_add-card">
           <button
-            class="popup__close-button popup__close-button_type_add-card"
+            className="popup__close-button popup__close-button_type_add-card"
             type="button"
           ></button>
-          <h3 class="popup__heading">Новое место</h3>
+          <h3 className="popup__heading">Новое место</h3>
           <form
-            class="popup__form popup__form_type_add-card"
+            className="popup__form popup__form_type_add-card"
             name="add-card"
             novalidate
           >
-            <div class="popup__input-container">
+            <div className="popup__input-container">
               <input
                 name="card-name-input"
                 id="card-name-input"
-                class="popup__input popup__input_type_card-name"
+                className="popup__input popup__input_type_card-name"
                 type="text"
                 placeholder="Название"
                 minlength="2"
@@ -122,25 +122,25 @@ function App() {
               />
               <span
                 id="card-name-input-error"
-                class="popup__input-error popup__input-error_visible"
+                className="popup__input-error popup__input-error_visible"
               ></span>
             </div>
-            <div class="popup__input-container">
+            <div className="popup__input-container">
               <input
                 name="card-url-input"
                 id="card-url-input"
-                class="popup__input popup__input_type_card-link"
+                className="popup__input popup__input_type_card-link"
                 type="url"
                 placeholder="Ссылка на картинку"
                 required
               />
               <span
                 id="card-url-input-error"
-                class="popup__input-error popup__input-error_visible"
+                className="popup__input-error popup__input-error_visible"
               ></span>
             </div>
             <button
-              class="popup__save-button popup__save-button_type_add-card"
+              className="popup__save-button popup__save-button_type_add-card"
               type="submit"
             >
               Создать
@@ -149,34 +149,34 @@ function App() {
         </div>
       </div>
 
-      <div class="popup popup_type_change-avatar">
-        <div class="popup__container">
+      <div className="popup popup_type_change-avatar">
+        <div className="popup__container">
           <button
-            class="popup__close-button"
+            className="popup__close-button"
             type="button"
           ></button>
-          <h3 class="popup__heading">Обновить аватар</h3>
+          <h3 className="popup__heading">Обновить аватар</h3>
           <form
-            class="popup__form popup__form_type_change-avatar"
+            className="popup__form popup__form_type_change-avatar"
             name="change-avatar"
             novalidate
           >
-            <div class="popup__input-container">
+            <div className="popup__input-container">
               <input
                 name="avatar-url-input"
                 id="avatar-url-input"
-                class="popup__input popup__input_type_avatar-url"
+                className="popup__input popup__input_type_avatar-url"
                 type="url"
                 placeholder="Ссылка на картинку"
                 required
               />
               <span
                 id="avatar-url-input-error"
-                class="popup__input-error popup__input-error_visible"
+                className="popup__input-error popup__input-error_visible"
               ></span>
             </div>
             <button
-              class="popup__save-button popup__save-button_type_change-avatar"
+              className="popup__save-button popup__save-button_type_change-avatar"
               type="submit"
             >
               Сохранить
@@ -186,31 +186,31 @@ function App() {
       </div>
     
 
-    <div class="popup popup_type_img-open">
-      <div class="popup__box">
+    <div className="popup popup_type_img-open">
+      <div className="popup__box">
         <button
-          class="popup__close-button popup__close-button_type_img-open"
+          className="popup__close-button popup__close-button_type_img-open"
           type="button"
         ></button>
-        <img class="popup__image" alt="картинка" src="#" />
-        <h3 class="popup__subtitle"></h3>
+        <img className="popup__image" alt="картинка" src="#" />
+        <h3 className="popup__subtitle"></h3>
       </div>
     </div>
 
-    <div class="popup popup_type_confirm">
-      <div class="popup__container">
+    <div className="popup popup_type_confirm">
+      <div className="popup__container">
         <button
-          class="popup__close-button"
+          className="popup__close-button"
           type="button"
         ></button>
-        <h3 class="popup__heading">Вы уверены?</h3>
+        <h3 className="popup__heading">Вы уверены?</h3>
         <form
-          class="popup__form"
+          className="popup__form"
           name="confirm"
           novalidate
         >
           <button
-            class="popup__save-button"
+            className="popup__save-button"
             type="submit"
           >
             Да
